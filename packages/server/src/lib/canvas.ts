9bab1ba14b518e06c1b25bb4be1b343ee4481ce0
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-01-29 15:23:47 +0100

    feat(api): Canvas class and update support

--- packages/server/src/lib/canvas.ts
+++ packages/server/src/lib/canvas.ts
@@ -1,10 +1,64 @@
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
+import { writeFileSync } from 'node:fs';
 
-export type Canvas = string[][];
+export type T_Canvas = string[][];
 
-export const canvas: Canvas = JSON.parse(
-  readFileSync(
-    fileURLToPath(new URL('canvas.json', import.meta.url))
-  ).toString()
-);
+export class Canvas {
+  private static instance: Canvas;
+
+  private _canvas!: T_Canvas;
+
+  constructor() {
+    this.canvas = JSON.parse(
+      readFileSync(
+        fileURLToPath(new URL('canvas.json', import.meta.url))
+      ).toString()
+    );
+  }
+
+  public static getInstance(): Canvas {
+    if (!Canvas.instance) {
+      Canvas.instance = new Canvas();
+    }
+
+    return Canvas.instance;
+  }
+
+  get canvas(): T_Canvas {
+    return this._canvas;
+  }
+
+  set canvas(canvas: T_Canvas) {
+    this._canvas = canvas;
+    this.save_canvas(); // backup to the file
+  }
+
+  set_pixel(x: number, y: number, color: string): void {
+    if (this._canvas === undefined) {
+      throw new Error('Canvas not initialized');
+    }
+
+    if (x < 0 || x >= this._canvas[0].length) {
+      throw new Error('x is out of bounds');
+    }
+
+    if (y < 0 || y >= this._canvas.length) {
+      throw new Error('y is out of bounds');
+    }
+
+    this._canvas[x][y] = color;
+    this.save_canvas(); // backup to the file
+  }
+
+  private save_canvas(): void {
+    if (this._canvas === undefined) {
+      throw new Error('Canvas not initialized');
+    }
+
+    writeFileSync(
+      fileURLToPath(new URL('canvas.json', import.meta.url)),
+      JSON.stringify(this._canvas)
+    )
+  }
+}
--- packages/server/src/routes/canvas.ts
+++ packages/server/src/routes/canvas.ts
@@ -1,8 +1,9 @@
 import { Response } from 'fets';
 import { router } from '../lib/server.js';
-import { canvas } from '../lib/canvas.js';
+import { Canvas } from '../lib/canvas.js';
 
 const CANVAS_PATH = '/canvas';
+const canvas = Canvas.getInstance();
 
 router.route({
   path: CANVAS_PATH,
@@ -21,6 +22,36 @@ router.route({
     }
   },
   handler() {
-    return Response.json(canvas);
+    return Response.json(canvas.canvas);
+  }
+});
+
+router.route({
+  path: CANVAS_PATH,
+  method: 'POST',
+  schemas: {
+    json: {
+      type: 'object',
+      properties: {
+        x: { type: 'integer' },
+        y: { type: 'integer' },
+        color: { type: 'string' }
+      },
+      additionalProperties: false,
+      required: ['x', 'y', 'color']
+    },
+  },
+  handler: async request => {
+    const { x, y, color } = await request.json();
+    try {
+      canvas.set_pixel(x, y, color);
+    } catch (error: any) {
+      return Response.json(
+        { message: error.message },
+        { status: 401 }
+      );
+    }
+
+    return Response.json(canvas.canvas);
   }
 });
